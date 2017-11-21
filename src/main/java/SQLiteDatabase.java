f01c595358bd7d9eaf206efb4fd3f2860f21aaab
Notty <tatpong.w@ku.th>
2017-11-21 16:28:07 +0700

    by Nott

--- src/main/java/SQLiteDatabase.java
+++ src/main/java/SQLiteDatabase.java
@@ -1,14 +1,14 @@
-public class SQLiteDatabase {
-    public String url = "SeedProduct.db";
-
-    private void prepareConnection(){
-        try {
-            Class.forName("org.sqlite.JDBC");
-            String dbURL = "jdbc:sqlite:" + url;
-            //Connection conn = DriverManager.getConnection(dbURL);
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-}
+//public class SQLiteDatabase {
+//    public String url = "SeedProduct.db";
+//
+//    private void prepareConnection(){
+//        try {
+//            Class.forName("org.sqlite.JDBC");
+//            String dbURL = "jdbc:sqlite:" + url;
+//            //Connection conn = DriverManager.getConnection(dbURL);
+//
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
