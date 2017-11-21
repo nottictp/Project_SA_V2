542c5d67899642a57ca0ffb8c9d190cbde0cf2d8
kampanatkr <kampanat.ru@ku.th>
2017-11-21 14:12:46 +0700

    by God

--- src/main/java/SQLiteDatabase.java
+++ src/main/java/SQLiteDatabase.java
@@ -1,16 +1,14 @@
-//import sun.rmi.transport.Connection;
-//
-//public class SQLiteDatabase {
-//    public String url = "SeedProduct.db";
-//
-//    private Connection prepareConnection(){
-//        try {
-//            Class.forName("org.sqlite.JDBC");
-//            String dbURL = "jdbc:sqlite:" + url;
-//            //Connection conn = DriverManager.getConnection(dbURL);
-//
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+public class SQLiteDatabase {
+    public String url = "SeedProduct.db";
+
+    private void prepareConnection(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            String dbURL = "jdbc:sqlite:" + url;
+            //Connection conn = DriverManager.getConnection(dbURL);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
