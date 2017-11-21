5e97944e1508319136fcdae7806caf2088d26638
Notty <tatpong.w@ku.th>
2017-11-21 21:56:10 +0700

    Nott: Update SQLiteDatabase

--- src/main/java/SQLiteDatabase.java
+++ src/main/java/SQLiteDatabase.java
@@ -1,14 +1,22 @@
-//public class SQLiteDatabase {
-//    public String url = "SeedProduct.db";
-//
-//    private void prepareConnection(){
-//        try {
-//            Class.forName("org.sqlite.JDBC");
-//            String dbURL = "jdbc:sqlite:" + url;
-//            //Connection conn = DriverManager.getConnection(dbURL);
-//
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLiteDatabase {
+    public String url = "SeedProduct.db";
+
+    private Connection prepareConnection(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            String dbURL = "jdbc:sqlite:" + url;
+            Connection conn = DriverManager.getConnection(dbURL);
+            return conn;
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
