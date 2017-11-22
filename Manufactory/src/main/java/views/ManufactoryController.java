b7451de43e558f70978394b1e64caada45cc32b1
kampanatkr <kampanat.ru@ku.th>
2017-11-22 20:26:54 +0700

    edit Marketing & Manufactory by god

--- Manufactory/src/main/java/views/ManufactoryController.java
+++ Manufactory/src/main/java/views/ManufactoryController.java
@@ -1,12 +1,37 @@
 package views;
 
 import controllers.MainController;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ManufactoryController implements Initializable{
+
+    @FXML private DatePicker datePicker;
+    @FXML private ComboBox tab1RecorderCombo;
+    @FXML private ComboBox lotNoCombo;
+    @FXML private ComboBox typeCombo;
+    @FXML private ComboBox unitCombo;
+    @FXML private ComboBox tab2RecorderCombo;
+    @FXML private TextField amountField;
+    @FXML private Button farmerSearchBtn;
+    @FXML private Button submitBtn;
+    @FXML private Button saveBtn;
+    @FXML private Button cancelBtn;
+    @FXML private TableView tab1DataTable;
+    @FXML private TableView tab2DataTable;
+    @FXML private TableColumn groupColumn;
+    @FXML private TableColumn orderColumn;
+    @FXML private TableColumn tab1IDColumn;
+    @FXML private TableColumn tab2IDColumn;
+    @FXML private TableColumn tab1nameColumn;
+    @FXML private TableColumn tab2nameColumn;
+    @FXML private TableColumn purchaseColumn;
+    @FXML private TableColumn unitColumn;
+    @FXML private TableColumn areaColumn;
 
     MainController controller;
 
--- Marketing/src/main/java/views/MarketingController.java
+++ Marketing/src/main/java/views/MarketingController.java
@@ -1,12 +1,20 @@
 package views;
 
 import controllers.MainController;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MarketingController implements Initializable {
+    @FXML private ComboBox typeCombo;
+    @FXML private ComboBox unitCombo;
+    @FXML private TextField amountField;
+    @FXML private Button submitOrderBtn;
     private MainController controller;
     public void initialize(URL location, ResourceBundle resources) {
 
