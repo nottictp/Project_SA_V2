2cbf66b420476d7f592a950cbc0b6fb28d1d7782
Notty <tatpong.w@ku.th>
2017-12-15 13:47:14 +0700

    Notty Manu db

--- Manufactory/src/main/java/views/TabDistributed.java
+++ Manufactory/src/main/java/views/TabDistributed.java
@@ -1,4 +1,25 @@
 package views;
 
-public class TabDistributed  {
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.Serializable;
+
+public class TabDistributed extends AnchorPane implements Serializable {
+
+    @FXML private DatePicker datePicker;
+    @FXML private TextField amountField;
+    @FXML private TextField tab1RecorderTextField;
+    @FXML private ComboBox unitCombo;
+    @FXML private ComboBox typeCombo;
+    @FXML private Button farmerSearchBtn;
+    @FXML private Button submitBtn;
+
+    @FXML private TableColumn groupColumn;
+    @FXML private TableColumn tab1IDColumn;
+    @FXML private TableColumn tab1nameColumn;
+    @FXML private TableColumn capacityColumn;
+
+
 }
--- Manufactory/src/main/java/views/TabSaveManufacture.java
+++ Manufactory/src/main/java/views/TabSaveManufacture.java
@@ -1,4 +1,10 @@
 package views;
 
-public class TabSaveManufacture {
+import javafx.scene.layout.AnchorPane;
+
+import java.io.Serializable;
+
+public class TabSaveManufacture extends AnchorPane implements Serializable{
+
+    
 }
