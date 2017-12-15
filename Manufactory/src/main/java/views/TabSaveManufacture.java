e3c7bbf5ba4b9fa7bdc893c2a72811a80b9656e4
Notty <tatpong.w@ku.th>
2017-12-15 15:53:48 +0700

    Notty อะไรไม่รู้

--- Manufactory/src/main/java/views/TabSaveManufacture.java
+++ Manufactory/src/main/java/views/TabSaveManufacture.java
@@ -1,10 +1,17 @@
 package views;
 
+import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.Serializable;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class TabSaveManufacture extends AnchorPane implements Serializable{
+public class TabSaveManufacture extends AnchorPane implements Initializable{
 
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
